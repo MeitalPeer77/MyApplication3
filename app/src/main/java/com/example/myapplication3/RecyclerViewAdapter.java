8f25500a588464d9bb9780289c4cddd013e48870
meitalpeer <meital.peer@mail.huji.ac.il>
2019-05-01 18:34:19 +0300

    seprating suggestions and partners

--- app/src/main/java/com/example/myapplication3/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/myapplication3/RecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.myapplication3;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -85,8 +86,6 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             distance =itemView.findViewById(R.id.distance);
             Pace = itemView.findViewById(R.id.running_pace);
             parentLayout=itemView.findViewById(R.id.parent_layout);
-
-
         }
     }
 
